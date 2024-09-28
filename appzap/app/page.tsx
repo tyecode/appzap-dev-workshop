48bc7a28695862c51307108e7c33bb9ee0d0ad70
tyecode <sengphachanh.dev@gmail.com>
2024-09-28 12:16:22 +0700

    feat: add restaurant api

--- appzap/app/page.tsx
+++ appzap/app/page.tsx
@@ -4,8 +4,22 @@ import { RestaurantCard } from "@/components/RestaurantCard";
 
 import { RESTAURANTS } from "./data";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("api");
+      const data = await res.json();
+      setData(data);
+    };
+    fetchData();
+  }, []);
+
+  console.log(data);
+
   return (
     <section className="container pt-2 pb-6">
       <div className="w-full h-28 rounded-lg overflow-hidden flex-center">
@@ -19,8 +33,8 @@ export default function Home() {
       </div>
       <h1 className="text-base text-muted-foreground py-2">ລາຍການຮ້ານອາຫານ</h1>
       <div className="flex flex-col gap-4">
-        {RESTAURANTS.map((restaurant) => (
-          <RestaurantCard key={restaurant.id} data={restaurant} />
+        {data.map((restaurant: any, index: number) => (
+          <RestaurantCard key={index} data={restaurant} />
         ))}
       </div>
     </section>
--- appzap/components/RestaurantCard.tsx
+++ appzap/components/RestaurantCard.tsx
@@ -1,3 +1,4 @@
+import { API_ENDPOINT, IMAGE_ENDPOINT } from "@/constants";
 import { cn } from "@/utils/cn";
 import Image from "next/image";
 import Link from "next/link";
@@ -18,7 +19,9 @@ interface RestaurantCardProps {
   data: RestaurantData;
 }
 
-export const RestaurantCard: React.FC<RestaurantCardProps> = ({ data }) => {
+export const RestaurantCard: React.FC<RestaurantCardProps> = ({
+  data,
+}: any) => {
   const {
     id,
     coverImage,
@@ -34,7 +37,7 @@ export const RestaurantCard: React.FC<RestaurantCardProps> = ({ data }) => {
     <div className="w-full rounded-lg overflow-hidden shadow-custom-card">
       <div className="flex-center w-full h-[120px]">
         <Image
-          src={coverImage}
+          src={IMAGE_ENDPOINT + data.storeDetails.image}
           alt="Restaurant"
           className="w-full h-full object-cover"
           width={480}
@@ -46,7 +49,7 @@ export const RestaurantCard: React.FC<RestaurantCardProps> = ({ data }) => {
       <div className="container flex gap-2 pb-2">
         <div className="w-[60px] h-[60px] rounded-full -mt-5 border-2 border-white relative bg-white">
           <Image
-            src={profileImage}
+            src={IMAGE_ENDPOINT + data.storeDetails.image}
             alt="Profile"
             className="w-full h-full object-cover rounded-full"
             width={100}
@@ -57,26 +60,28 @@ export const RestaurantCard: React.FC<RestaurantCardProps> = ({ data }) => {
           <div
             className={cn(
               "w-4 h-4 rounded-full absolute right-0 bottom-0 bg-white border-2 border-white",
-              isActive ? "bg-green-400" : "bg-gray-300"
+              data.storeDetails.isOpen ? "bg-green-400" : "bg-gray-300"
             )}
           ></div>
         </div>
         <div className="flex w-full flex-row gap-2 flex-wrap flex-1">
           <div className="py-2 flex flex-col gap-2 flex-1 min-w-40">
             <h2 className="text-foreground text-base font-bold">
-              {restaurantName}
+              {data.storeDetails.name}
             </h2>
             <p className="text-sm text-muted-foreground font-normal flex gap-1">
               <span className="text-primary font-semibold flex items-center gap-1.5">
                 <FaToriiGate />
-                ໂຕະວ່າງ {availableTables}
+                ໂຕະວ່າງ {data.activeCount}
               </span>
-              <span>/ {totalTables}</span>
+              <span>/ {data.totalTables}</span>
             </p>
-            <p className="text-xs text-muted-foreground">{operatingHours}</p>
+            <p className="text-xs text-muted-foreground">
+              {data.storeDetails.note}
+            </p>
           </div>
           <div className="w-[100px] py-4">
-            {isActive ? (
+            {data.storeDetails.isOpen ? (
               <Link href={`/booking/${id}`}>
                 <button className="w-full h-10 border rounded-lg border-primary flex-center text-primary text-base shadow-sm">
                   ຈອງໂຕະ
